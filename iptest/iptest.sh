ccc73d0e18a16d439ad0956f178eeab36ad9c65b
rqysir609 <rqysir609@gmail.com>
2024-05-05 15:48:34 +0800

    dsjipaofjoaj

--- iptest/iptest.sh
+++ iptest/iptest.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-./CloudflareST -f ip.txt -tl 500 -sl 5 -url https://speedtest.venusir.com
+git pull
+
+./CloudflareST -f ip_hk.txt -tl 500 -sl 5 -url https://speedtest.venusir.com
+
+git add .
+
+git commit . -m 每日更新优选IP
+
+git push
