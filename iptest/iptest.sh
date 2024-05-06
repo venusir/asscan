11735df58990e033cb002969977eeabc61908a1f
rqysir609 <rqysir609@gmail.com>
2024-05-06 23:33:20 +0800

    Update README.md

--- iptest/iptest.sh
+++ iptest/iptest.sh
@@ -2,7 +2,23 @@
 
 git pull
 
-./CloudflareST -f ip_hk.txt -tl 500 -sl 5 -url https://speedtest.venusir.com
+cat /dev/null > cfproxy.txt
+
+# 筛选HK IP
+./CloudflareST -f ip_hk.txt -tl 250 -sl 5 -url https://speedtest.venusir.com
+
+awk -F "," 'NR!=1{print $1}' result.csv > tmp.txt
+
+sed "s/$/:443#HK/g" tmp.txt >> cfproxy.txt
+
+# 筛选CN IP
+./CloudflareST -f ip_cn.txt -tl 250 -sl 5 -url https://speedtest.venusir.com
+
+awk -F "," 'NR!=1{print $1}' result.csv > tmp.txt
+
+sed "s/$/:443#CN/g" tmp.txt >> cfproxy.txt
+
+rm -rf tmp.txt
 
 git add .
 
