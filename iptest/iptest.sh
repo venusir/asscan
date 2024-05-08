c0f6c201bd7aa3f29f7211cbe3324916b1adf3f0
venusir <rqysir609@gmail.com>
2024-05-08 14:59:27 +0000

    vfjsdlfs

--- iptest/iptest.sh
+++ iptest/iptest.sh
@@ -7,18 +7,18 @@ cat /dev/null > cfproxy.txt
 chmod +x CloudflareST
 
 # 筛选HK IP
+./CloudflareST -f ip.txt -tl 250 -sl 5 -url https://speedtest.venusir.com
+
+awk -F "," 'NR!=1{print $1}' result.csv > tmp.txt
+
+sed "s/$/:443#CF/g" tmp.txt >> cfproxy.txt
+
+# 筛选CN IP
 ./CloudflareST -f ip_hk.txt -tl 250 -sl 5 -url https://speedtest.venusir.com
 
 awk -F "," 'NR!=1{print $1}' result.csv > tmp.txt
 
 sed "s/$/:443#HK/g" tmp.txt >> cfproxy.txt
-
-# 筛选CN IP
-./CloudflareST -f ip_cn.txt -tl 250 -sl 5 -url https://speedtest.venusir.com
-
-awk -F "," 'NR!=1{print $1}' result.csv > tmp.txt
-
-sed "s/$/:443#CN/g" tmp.txt >> cfproxy.txt
 
 rm -rf tmp.txt
 rm -rf result.csv
