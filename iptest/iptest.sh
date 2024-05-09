856873f3cf4ae91e7ee365edf9a3d5302d631f42
rqysir609 <rqysir609@gmail.com>
2024-05-09 22:39:51 +0800

    vfjsdlfs

--- iptest/iptest.sh
+++ iptest/iptest.sh
@@ -8,10 +8,10 @@
 
 for ips in $(find ips -name "*.txt") 
 do
-  echo $(basename $ips .txt) | awk '{print toupper($0)}'
+  asn=$(basename $ips .txt) | awk '{print toupper($0)}'
   #./CloudflareST -f $ips -tl 250 -sl 5 -dn 5 -url https://speedtest.venusir.com
   #awk -F "," 'NR!=1{print $1}' result.csv > tmp.txt
-  #sed "s/$/:443#${asn}/g" tmp.txt >> proxy.txt
+  sed 's/$/:443#'$asn'/g' tmp.txt >> proxy.txt
 done;
 
 #rm -rf tmp.txt
