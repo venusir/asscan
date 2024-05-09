2c754f20971e2969a401f14e9f757d3c92859d8f
rqysir609 <rqysir609@gmail.com>
2024-05-09 22:08:30 +0800

    vfjsdlfs

--- iptest/iptest.sh
+++ iptest/iptest.sh
@@ -1,24 +1,23 @@
 #!/bin/bash
 
-git pull
+#git pull
 
-chmod +x CloudflareST
+#chmod +x CloudflareST
 
-cat /dev/null > proxy.txt
+#cat /dev/null > proxy.txt
 
 for ips in $(find ips -name "*.txt") 
 do 
-  echo $(basename $ips)
+  asn=$(basename $ips .txt) | awk '{print toupper($0)}'
+  echo $asn
   #./CloudflareST -f $ips -tl 250 -sl 5 -dn 5 -url https://speedtest.venusir.com
   #awk -F "," 'NR!=1{print $1}' result.csv > tmp.txt
-  #sed "s/$/:443#CF/g" tmp.txt >> proxy.txt
+  #sed "s/$/:443#${asn}/g" tmp.txt >> proxy.txt
 done;
 
-rm -rf tmp.txt
-rm -rf result.csv
+#rm -rf tmp.txt
+#rm -rf result.csv
 
-git add .
-
-git commit . -m 每日更新优选IP
-
-git push
+#git add .
+#git commit . -m 每日更新优选IP
+#git push
