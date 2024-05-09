d601e722b221e5fe2c819ea24ad17415819c2070
rqysir609 <luntan609@hotmail.com>
2024-05-09 15:26:48 +0800

    vfjsdlfs

--- iptest/iptest.sh
+++ iptest/iptest.sh
@@ -9,9 +9,9 @@ cat /dev/null > proxy.txt
 for ips in `find ips -name ".txt"` 
 do 
   echo $ips
-  ./CloudflareST -f ips -tl 250 -sl 5 -dn 5 -url https://speedtest.venusir.com
-  awk -F "," 'NR!=1{print $1}' result.csv > tmp.txt
-  sed "s/$/:443#CF/g" tmp.txt >> proxy.txt
+  #./CloudflareST -f $ips -tl 250 -sl 5 -dn 5 -url https://speedtest.venusir.com
+  #awk -F "," 'NR!=1{print $1}' result.csv > tmp.txt
+  #sed "s/$/:443#CF/g" tmp.txt >> proxy.txt
 done;
 
 rm -rf tmp.txt
